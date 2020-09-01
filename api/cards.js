039d7821b8a68d411c164513cd54d9bb384ccbbb
Matías Menich <matimenich@gmail.com>
2020-08-31 21:31:56 -0400

    Cors helper function

--- api/cards.js
+++ api/cards.js
@@ -1,21 +1,5 @@
 const kushkiAPI = require("../src/kushkiAPI");
-
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
+const allowCors = require("../src/allowCors");
 
 const handler = async (req, res) => {
   const token = req.body.token;
